12b58f44b1d5640360f5252fd469ba37836e29fa
Natalka-01 <nataliia.dubikova@gmail.com>
2025-10-26 18:10:46 +0100

    gallary layout done main

---
+++ src/js/render-functions.js
@@ -0,0 +1,11 @@
+// export const createGalleryCardTemplate = imgInfo => {
+  
+
+//   return `
+//             <li class="gallery-card">
+//             <img class="gallery-img" src="${imgInfo.hits.webformatURL}" alt="${imgInfo.hits.tags}" />
+//                 <span class="label">Views</span>
+//                 <span class="value">900290</span>
+//             </li>
+//             `
+// };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,43 @@
+
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+// import { createGalleryCardTemplate } from '.js/render-functions';
+
+const createGalleryCardTemplate = imgInfo => {
+  
+
+  return `
+
+    <li class="gallery-card">
+    <img class="gallery-img" src="${imgInfo.webformatURL}" alt="${imgInfo.tags}" data-large-img = "${imgInfo.largeImageURL}"/>
+    <div class="stats-container">
+        <ul class="stats-list">
+            <li class="gallery-item">
+                <span class="label">Likes</span>
+                <span class="value">${imgInfo.likes}</span>
+            </li>
+            <li class="gallery-item">
+                <span class="label">Views</span>
+                <span class="value">${imgInfo.views}</span>
+            </li>
+            <li class="gallery-item">
+                <span class="label">Comments</span>
+                <span class="value">${imgInfo.comments}</span>
+            </li>
+            <li class="gallery-item">
+                <span class="label">Downloads</span>
+                <span class="value">${imgInfo.downloads}</span>
+            </li>
+        </ul>
+    </div>
+</li>`
+};
+
 //TODO1 Форма пошуку
 const refs = {
-    searchForm: document.querySelector('.js-search-form')
+    searchForm: document.querySelector('.js-search-form'),
+    gallery: document.querySelector('.js-gallery')
 }
 
 const onSearchFormSubmit = event => {
@@ -12,12 +49,50 @@ const onSearchFormSubmit = event => {
     
 
     if (searchedQuery.length === 0) {
-        alert("It can't me empty!")
+        
+         iziToast.show({
+    title: "WARRNING",
+    message: `It can't be empty!`,
+    color: 'red', // blue, red, green, yellow
+    position: 'topCenter', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+
+});
 
         return;
     }
 
     console.log(searchedQuery)
+
+    fetch(
+        `https://pixabay.com/api/?key=52947144-373b760a7dc07b63f24b6c37a&q=${searchedQuery}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then (response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+        if (data.hits.length === 0){
+            iziToast.show({
+    message: 'Sorry, there are no images matching your search query. Please try again!',
+    color: 'red', // blue, red, green, yellow
+    position: 'topCenter', // bottomRight, bottomLeft, topRight, topLeft, topCenter, bottomCenter, center
+
+});
+    return
+        }
+
+        const galleryCardTemplate = data.hits.map(pictureInfo => createGalleryCardTemplate(pictureInfo)).join('')
+        refs.gallery.innerHTML = galleryCardTemplate
+    })
+    .catch(err => {
+        console.log(err);
+    });
 }
 
 refs.searchForm.addEventListener('submit', onSearchFormSubmit)
+
+
+//TODO_2 HTTP-запити
